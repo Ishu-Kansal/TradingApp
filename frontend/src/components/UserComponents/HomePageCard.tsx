0e26414c101c4693009f0e2c1cff657ab5f5c26e
Ishu-Kansal <ikansal22@gmail.com>
2025-05-16 23:32:53 -0400

    added user home page improvements

---
+++ frontend/src/components/UserComponents/HomePageCard.tsx
@@ -0,0 +1,27 @@
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import "../../styles/UserStyles/components/HomePageCard.css";
+
+interface HomePageCardProps {
+  description: string;
+  img_url: string;
+  path_url: string;
+}
+
+function HomePageCard(props: HomePageCardProps) {
+  return (
+    <div className="HomePageCard">
+      <div>
+        {" "}
+        <Link to={props.path_url} id="hcard-top-text">
+          {props.description}
+        </Link>
+      </div>
+      <div
+        id="hcard-bottom-img"
+        style={{ backgroundImage: `url(${props.img_url})` }}
+      />
+    </div>
+  );
+}
+
+export default HomePageCard;
--- frontend/src/pages/UserPages/Home.tsx
+++ frontend/src/pages/UserPages/Home.tsx
@@ -1,16 +1,46 @@
-import { Button } from "@mui/material";
 import "../../styles/UserStyles/pages/Home.css";
+import HomePageCard from "../../components/UserComponents/HomePageCard";
+
+import iv_graph_img from "../../assets/iv_graph.png";
+import options_tables_img from "../../assets/options_tables.png";
+import orderbook_img from "../../assets/orderbook.png";
+import profit_calc_img from "../../assets/profit_calc.png";
 
 function Home() {
   return (
-    <div className="buttons">
-      <Button variant="contained" href="/options-tables">
-        Options Tables
-      </Button>
-
-      <Button variant="contained" href="/bid-ask-test">
-        Bid-Ask DB
-      </Button>
+    <div className="homepage">
+      <div id="title">
+        {" "}
+        <h1 id="head">Welcome to TradeSquare, {`{username}!`}</h1>
+        <h1 id="subhead">Explore our offerings below</h1>
+      </div>
+      <div id="grid">
+        <HomePageCard
+          description="options tables"
+          img_url={options_tables_img}
+          path_url="/options-tables"
+        />
+        <HomePageCard
+          description="iv graphs"
+          img_url={iv_graph_img}
+          path_url="/hist-iv-charts"
+        />
+        <HomePageCard
+          description="orderbooks"
+          img_url={orderbook_img}
+          path_url="/bid-ask-test"
+        />
+        <HomePageCard
+          description="profit calc"
+          img_url={profit_calc_img}
+          path_url="/profit-calc"
+        />
+        <HomePageCard
+          description="portfolio"
+          img_url={options_tables_img}
+          path_url="/portfolio"
+        />
+      </div>
     </div>
   );
 }
